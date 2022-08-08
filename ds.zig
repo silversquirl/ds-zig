8ff4bfaf45143cf83b54f3f8a963348720f27948
Silver <silver@silv.re>
2022-08-08 17:40:06 +0100

    fix: expose datastructures

--- ds.zig
+++ ds.zig
@@ -1,6 +1,6 @@
-const AvlTree = @import("avl_tree.zig").AvlTree;
-const IntervalTree = @import("interval_tree.zig").IntervalTree;
-const LinkedHeap = @import("linked_heap.zig").LinkedHeap;
+pub const AvlTree = @import("avl_tree.zig").AvlTree;
+pub const IntervalTree = @import("interval_tree.zig").IntervalTree;
+pub const LinkedHeap = @import("linked_heap.zig").LinkedHeap;
 
 comptime {
     _ = AvlTree;
